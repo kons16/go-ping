cc89fb4ff9bf8931dd2d312b696281d3324610e4
kons16 <ks1216r@gmail.com>
2021-09-22 22:25:39 +0900

    implement func

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"net"
@@ -8,29 +9,129 @@ import (
 	"time"
 )
 
+type Type uint8
+
+const (
+	ECHO_REPLY Type = 0
+	ECHO       Type = 8
+)
+
+type IcmpHdr struct {
+	Type     Type
+	Code     uint8
+	Checksum uint16
+	ID       uint16
+	Seq      uint16
+	Data     []byte
+}
+
+func (m *IcmpHdr) Marshal() []byte {
+	b := make([]byte, 8+len(m.Data))
+	b[0] = byte(m.Type)
+	b[1] = byte(m.Code)
+	b[2] = 0
+	b[3] = 0
+	binary.BigEndian.PutUint16(b[4:6], m.ID)
+	binary.BigEndian.PutUint16(b[6:8], m.Seq)
+	copy(b[8:], m.Data)
+	cs := checkSum(b)
+	b[2] = byte(cs >> 8)
+	b[3] = byte(cs)
+	return b
+}
+
 // checkSum はチェックサムの計算を行う
-func checkSum() {}
+func checkSum(b []byte) uint16 {
+	count := len(b)
+	sum := uint32(0)
+	for i := 0; i < count-1; i += 2 {
+		sum += uint32(b[i])<<8 | uint32(b[i+1])
+	}
+	if count&1 != 0 {
+		sum += uint32(b[count-1]) << 8
+	}
+	for (sum >> 16) > 0 {
+		sum = (sum & 0xffff) + (sum >> 16)
+	}
+	return ^(uint16(sum))
+
+}
 
 // sendPing は ping を送信する
-func sendPing(conn net.Conn, name string, len int, sqc int, sendTime *time.Time) int {}
+func sendPing(conn net.Conn, name string, len int, sqc uint16, sendTime *time.Time) int {
+	tb, err := time.Now().MarshalBinary()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Time.MarshalBinary:", err)
+		os.Exit(1)
+	}
+
+	m := IcmpHdr{
+		Type: ECHO,
+		Code: 0,
+		ID:   uint16(os.Getpid() & 0xffff),
+		Seq:  sqc,
+		Data: tb,
+	}
+
+	mb := m.Marshal()
+	_, err = conn.Write(mb)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Write:", err)
+		os.Exit(1)
+	}
+
+	return 0
+}
 
 // checkPacket は受信したパケットを確認する
-func checkPacket() {}
+func checkPacket(b []byte) int {
+	hlen := int(b[0]&0x0f) << 2
+
+	b = b[hlen:]
+	m := &IcmpHdr{
+		Type:     Type(b[0]),
+		Code:     uint8(b[1]),
+		Checksum: uint16(binary.BigEndian.Uint16(b[2:4])),
+		ID:       uint16(binary.BigEndian.Uint16(b[4:6])),
+		Seq:      uint16(binary.BigEndian.Uint16(b[6:8])),
+	}
+	m.Data = make([]byte, len(b)-8)
+	copy(m.Data, b[8:])
+
+	// fmt.Fprintf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f ms\n",nbytes-iph->ihl*4,inet_ntoa(from->sin_addr),sqc,*ttl,*diff*1000.0)
+	fmt.Println("seq = ", m.Seq)
+	return 0
+}
 
 // recvPing は ping 応答を受信する
-func recvPing(conn net.Conn, len int, sqc int, sendTime *time.Time, timeOutSec int) int {}
+func recvPing(conn net.Conn, len int, sqc uint16, sendTime *time.Time, timeOutSec int) int {
+	rb := make([]byte, 100)
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+
+	n, err := conn.Read(rb)
+	if err != nil {
+		return -2000
+	}
+
+	ret := checkPacket(rb[:n])
+	if ret == 0 {
+		return 0
+	} else {
+		return 1
+	}
+}
 
 // pingCheck は ping の送受信を行う
 // name 疎通確認先, len ICMPパケット長, times ICMP送受信繰り返し回数, timeOutSec 応答受信待ち秒数
-func pingCheck(name string, len int, times int, timeOutSec int) (int, error) {
-	var ip string
+func pingCheck(ipStr string, len int, times int, timeOutSec int) (int, error) {
 	var sendTime time.Time
 	var ret int
 	var total, totalNo int
 
 	// ソケットを作成
-	conn, err := net.Dial("ip4:1", ip)
+	conn, err := net.Dial("ip4:1", ipStr)
 	if err != nil {
+		fmt.Println("aaa")
 		fmt.Sprintf("%s", err)
 		return -1, err
 	}
@@ -38,10 +139,10 @@ func pingCheck(name string, len int, times int, timeOutSec int) (int, error) {
 
 	for i := 0; i < times; i++ {
 		// ICMPエコーリクエストを送信する
-		ret = sendPing(conn, name, len, i+1, &sendTime)
+		ret = sendPing(conn, ipStr, len, uint16(i+1&0xffff), &sendTime)
 		if ret == 0 {
 			// ICMPエコーリプライを受信する
-			ret = recvPing(conn, len, i+1, &sendTime, timeOutSec)
+			ret = recvPing(conn, len, uint16(i+1&0xffff), &sendTime, timeOutSec)
 			if ret >= 0 {
 				total += ret
 				totalNo++
@@ -60,10 +161,11 @@ func pingCheck(name string, len int, times int, timeOutSec int) (int, error) {
 
 func main() {
 	flag.Parse()
-	name := flag.Args()[0]
+	ipStr := flag.Args()[0]
 
-	ret, err := pingCheck(name, 64, 5, 1)
+	ret, err := pingCheck(ipStr, 64, 5, 1)
 	if err != nil {
+		fmt.Sprintf("%s", err)
 		os.Exit(1)
 	}
 
